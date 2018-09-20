500452ef1c0670efc01da5fd27e8a4bf7b5e72c3
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2018-09-20 13:17:01 -0400

    Setup HTML and connected to firebase

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCdg25ZPpIbD3uO1oVsQUhoNQbLTyurID0",
+        authDomain: "train-time-43114.firebaseapp.com",
+        databaseURL: "https://train-time-43114.firebaseio.com",
+        projectId: "train-time-43114",
+        storageBucket: "train-time-43114.appspot.com",
+        messagingSenderId: "57632226602"
+    };
+    firebase.initializeApp(config);
+    
+});
