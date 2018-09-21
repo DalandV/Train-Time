562d601d962583f7aa9186fbedc8aa49d28b1013
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2018-09-20 22:13:02 -0400

    can take in and store values for name, destination, time, and frequency

--- assets/script.js
+++ assets/script.js
@@ -10,5 +10,34 @@ $(document).ready(function () {
         messagingSenderId: "57632226602"
     };
     firebase.initializeApp(config);
-    
+
+    var database = firebase.database();
+
+    $("#submit-btn").on("click", function (event) {
+
+        event.preventDefault();
+
+        var tName = $("#train-name").val().trim();
+        var tDest = $("#destination").val().trim();
+        var tTime = $("#first-train-time").val().trim();
+        var tFreq = $("#frequency").val().trim();
+
+        database.ref().push({
+            trainName: tName,
+            trainDest: tDest,
+            trainTime: tTime,
+            trainFreq: tFreq
+        });
+
+    });
+
+    database.ref().on("child_added", function (snapshot) {
+
+        $("#train-sched").append("<tr><td>" + snapshot.val().trainName + "</td><td>" + snapshot.val().trainDest + "</td><td>" + snapshot.val().trainFreq + "</td></tr>");
+
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
+
 });
